8c0e33bc74b8da81f4af08cdace888c33f2fe59f
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-24 14:00:00 +0600

    creating vedio site with featching data

---
+++ script/video.js
@@ -0,0 +1,57 @@
+const loadCatagories=()=>{
+    const res=fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res =>res.json())
+    .then(data =>display(data.categories  ))
+    .catch((err)=>{
+        console.log('error occured here',err)
+    })
+}
+const loadVideos=()=>{
+    const res=fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res=>res.json())
+    .then(data =>displayVideos(data.videos))
+    .catch((err)=>{
+        console.log('error occured here',err)
+    })
+        
+}
+const displayVideos=(videos)=>{
+    const videoContainer=document.getElementById('videos')
+for(const video of videos){
+    console.log(video)
+    const card=document.createElement('div')
+    card.classList='card card-compact '
+    card.innerHTML=`
+    <figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+    <div><img class="h-10 w-10 rounded-full object-cover" src=${video.authors[0].profile_picture} alt="" /></div>
+  </div>
+  <div>
+  <h2 class="font-bold">${video.title}</h2>
+  <div class="flex gap-2 items-center">
+  <p class="text-gray-400">${video.authors[0].profile_name}</p>
+  <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="" />
+  </div>
+  
+  <p></p>
+  </div>
+    `
+    videoContainer.appendChild(card)
+}
+}
+const display=(catagoriesOfData)=>{
+    const category=document.getElementById('category')
+    for(const data of catagoriesOfData){
+        const button=document.createElement('button');
+        button.classList='btn'
+        button.innerText=data.category
+        category.appendChild(button);
+    }
+}
+loadCatagories();
+loadVideos();
